c91bc4d1be72b5928bd2fb73a37db15167acaec2
Mohsen Pouryazdan <mohs3n71@gmail.com>
2021-03-05 23:00:22 +0330

    added modal and loading componenets

--- components/form.tsx
+++ components/form.tsx
@@ -10,10 +10,10 @@ interface FormProps {
 const Form: FunctionComponent<FormProps> = ({ children, formTitle, onSubmit = (event) => { event.preventDefault() } }) => {
   return (
     <form className={styles.simpleForm} onSubmit={onSubmit}>
-      {formTitle && <h1 className="h4 mb-3 font-weight-normal">{formTitle}</h1>}
+      {formTitle && <h1 className="h4 mt-5 mb-3 font-weight-normal">{formTitle}</h1>}
       {children}
     </form>
-  );
+  );  
 }
 
 export default Form;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,51 @@
-import React, { useEffect, FunctionComponent } from 'react';
+import React, { FormEventHandler, FunctionComponent, useState } from 'react';
 import PageTemplate from 'components/pageTemplate';
 import HttpRequest from 'utils/HttpRequest';
 import Form from 'components/form';
 import Input from 'components/input';
 import Button from 'components/button';
+import Modal from 'components/modal';
+import Loading from 'components/loading';
 
 const Home: FunctionComponent = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const request = new HttpRequest('GET', 'http://localhost:3000/api/verify');
-    request.send();
-  }, []);
+  const onClose = (): void => {
+    setOpen(false);
+  };
+
+  const submitHandler: FormEventHandler = (event) => {
+    event.preventDefault();
+  }
 
   return (
-    <PageTemplate
-      pageTitle="Guess The Word"
-      footerDescription="You Can only play this game 2 times per day"
-    >
-      <div
-        className="d-flex w-100 h-100 justify-content-center align-items-center"
-        style={{
-          marginTop: 100,
-        }}>
-        <Form formTitle="Guess the word and submit">
-          <Input
-            inputID="wordInput"
-            labelText="Word"
-            placeholder="Enter Your Guess"
-            type="text" />
-          <Button type="submit">
-            Submit
+    <>
+      <Modal open={open} onClose={onClose}>
+      </Modal>
+      <PageTemplate
+        pageTitle="Guess The Word"
+        footerDescription="You Can only play this game 2 times per day"
+      >
+        <div
+          className="d-flex w-100 h-100 justify-content-center align-items-center mt-5">
+          <Form
+            formTitle="Guess the word and submit"
+            onSubmit={submitHandler}
+          >
+            <Input
+              inputID="wordInput"
+              labelText="Word"
+              placeholder="Enter Your Guess"
+              type="text" />
+            <Button type="submit">
+              Submit
           </Button>
-        </Form>
-      </div>
-    </PageTemplate>
+          </Form>
+        </div>
+        {loading && <Loading />}
+      </PageTemplate>
+    </>
   );
 }
 
